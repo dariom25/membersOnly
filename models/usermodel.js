f069436357ef2cc64797defab373904824dbd63a
dariom25 <dario@maciey.de>
2024-06-10 17:12:04 +0200

    Improve message property

--- models/usermodel.js
+++ models/usermodel.js
@@ -5,12 +5,11 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  messages: [Schema.Types.ObjectId],
+  messages: [{type: Schema.Types.ObjectId, ref: "Message"}],
   admin: { type: Boolean },
   email: { type: String, required: true },
   password: { type: String },
   hash: { type: String },
   salt: { type: String },
 });
-
 module.exports = mongoose.model("UserModel", UserSchema);
