f2eb4a886cfc95f595964f4f2fe98f98fa1e03ae
dariom25 <dario@maciey.de>
2024-06-10 22:10:04 +0200

    Refactor code

--- models/usermodel.js
+++ models/usermodel.js
@@ -5,11 +5,12 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  messages: [{type: Schema.Types.ObjectId, ref: "Message"}],
+  messages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
   admin: { type: Boolean },
   email: { type: String, required: true },
   password: { type: String },
   hash: { type: String },
   salt: { type: String },
 });
+
 module.exports = mongoose.model("UserModel", UserSchema);
