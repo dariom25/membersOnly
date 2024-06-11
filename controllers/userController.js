8d0b484fdc3f3e2afed0d0cc60bcf71ab7d8c811
dariom25 <dario@maciey.de>
2024-06-11 23:26:14 +0200

    Implement function to register users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,49 @@
-const User  = require("../models/usermodel")
-const asyncHandler = require("express-async-handler")
+const User = require("../models/usermodel");
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs")
 
 exports.createUserGet = asyncHandler(async (req, res, next) => {
-    res.render("registration")
-})
+  res.render("registration");
+});
+
+exports.createUserPost = [
+  body("firstName").trim().escape(),
+  body("lastName").trim().escape(),
+  body("email")
+    .isEmail()
+    .withMessage("Enter a valid email")
+    .custom(async (value) => {
+      const existingEmail = await User.findOne({ email: value }).exec();
+      if (existingEmail) {
+        throw new Error("Email already exists. Please use another one.")
+      }
+    })
+    .trim()
+    .escape(),
+  body("password").trim().escape(),
+  body("admin.*").escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req)
+
+    const salt = await bcrypt.genSalt(10)
+    const securedPassword = await bcrypt.hash(req.body.password, salt)
+
+    const user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        messages: [],
+        admin: req.body.admin === "Yes" ? true : false,
+        email: req.body.email,
+        password: securedPassword
+    })
+
+    if (!errors.isEmpty()) {
+        res.render("registration")
+    } else {
+      await user.save()
+      res.redirect("/")
+    }
+  })
+];
