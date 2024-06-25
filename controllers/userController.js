61836692bc645acd7486b34aa7ead1bba62e271d
dariom25 <dario@maciey.de>
2024-06-25 19:07:00 +0200

    Add validation for passwordmatching

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const User = require("../models/usermodel");
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
 
 exports.createUserGet = asyncHandler(async (req, res, next) => {
   res.render("registration");
@@ -16,39 +16,45 @@ exports.createUserPost = [
     .custom(async (value) => {
       const existingEmail = await User.findOne({ email: value }).exec();
       if (existingEmail) {
-        throw new Error("Email already exists. Please use another one.")
+        throw new Error("Email already exists. Please use another one.");
       }
     })
     .trim()
     .escape(),
   body("password").trim().escape(),
+  body("cpassword")
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage("Passwords need to match."),
+
   body("admin.*").escape(),
 
   asyncHandler(async (req, res, next) => {
-    const errors = validationResult(req)
+    const errors = validationResult(req);
 
-    const salt = await bcrypt.genSalt(10)
-    const securedPassword = await bcrypt.hash(req.body.password, salt)
+    const salt = await bcrypt.genSalt(10);
+    const securedPassword = await bcrypt.hash(req.body.password, salt);
 
     const user = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        messages: [],
-        admin: false,
-        member: false,
-        email: req.body.email,
-        password: securedPassword
-    })
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      messages: [],
+      admin: false,
+      member: false,
+      email: req.body.email,
+      password: securedPassword,
+    });
 
     if (!errors.isEmpty()) {
-        res.render("registration")
+      res.render("registration");
     } else {
-      await user.save()
-      res.redirect("registrationSuccess")
+      await user.save();
+      res.redirect("registrationSuccess");
     }
-  })
+  }),
 ];
 
-exports.successfullRegistrationGet = asyncHandler(async(req, res, next) => {
-  res.render("registrationSuccess")
-})
+exports.successfullRegistrationGet = asyncHandler(async (req, res, next) => {
+  res.render("registrationSuccess");
+});
