06eb786647eb9704d1c641874d576c4468b22031
dariom25 <dario@maciey.de>
2024-06-26 20:01:19 +0200

    Implement session middleware + localstrategy

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-require("dotenv").config()
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const User = require("./models/usermodel");
+const bcryptjs = require("bcryptjs");
+require("dotenv").config();
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 
 const app = express();
 
 //mongoose setup
 const mongoose = require("mongoose");
+const passport = require("passport");
 mongoose.set("strictQuery", false);
 const mongoDB = `mongodb+srv://dario:${process.env.DATABASE_KEY}@cluster0.uuebfsm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -31,7 +35,35 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+
+//express-session setup
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.session());
+
+//authenticate user
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ email: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      const match = bcryptjs.compare(passport, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
